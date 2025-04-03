0bd9b68373071e50121dcc653c8e342303ca15e7
kahnu044 <kahnucharan044@gmail.com>
2025-04-03 23:43:51 +0530

    wp_whois_lookup shortcode added

--- whois-lookup.php
+++ whois-lookup.php
@@ -23,3 +23,25 @@ function wp_whois_lookup_enqueue_scripts()
     wp_localize_script('wp-whois-script', 'wp_whois', array('ajaxurl' => admin_url('admin-ajax.php')));
 }
 add_action('wp_enqueue_scripts', 'wp_whois_lookup_enqueue_scripts');
+
+/**
+ * Shortcode to display the Whois lookup form and results.
+ *
+ * @return string HTML output of the form and results.
+ */
+function wp_whois_lookup_shortcode()
+{
+    ob_start();
+?>
+    <div class="wp-whois-lookup-wrapper">
+        <h2>Whois Lookup</h2>
+        <form id="whois-lookup-form">
+            <input type="text" id="wp-whois-domain" name="wp-whois-domain" placeholder="Enter domain name" required>
+            <button type="submit">Lookup</button>
+        </form>
+        <div id="wp-whois-results"></div>
+    </div>
+<?php
+    return ob_get_clean();
+}
+add_shortcode('wp_whois_lookup', 'wp_whois_lookup_shortcode');
