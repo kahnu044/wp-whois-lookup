3c34aa1f51dd7040af6e49305844b9a13b744093
kahnu044 <kahnucharan044@gmail.com>
2025-04-03 23:31:18 +0530

    Plugin Initialize

---
+++ whois-lookup.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WP Whois Lookup
+ * Plugin URI:  https://github.com/kahnu044/wp-whois-lookup
+ * Description: Provides a shortcode [wp_whois_lookup] to search for domain Whois information.
+ * Version:     1.0.0
+ * Author:      kahnu044
+ * Author URI:  https://github.com/kahnu044
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
