12ed68799c693bafee98a480c622995281b6facb
kahnu044 <kahnucharan044@gmail.com>
2025-04-04 00:02:41 +0530

    domain validation added

--- js/lookup.js
+++ js/lookup.js
@@ -1,3 +1,18 @@
 jQuery(document).ready(function ($) {
   console.log("wp_whois loaded........", wp_whois.ajaxurl);
+
+  function isValidDomain(domain) {
+    var domainRegex = /^(?!:\/\/)([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,10}$/;
+    return domainRegex.test(domain);
+  }
+
+  // Check if the domain is valid on keyup and change events
+  $("#wp-whois-domain").on("keyup change", function () {
+    var domain = $(this).val().trim();
+    if (domain === "" || isValidDomain(domain)) {
+      $("#wp-whois-results").html("");
+    } else {
+      $("#wp-whois-results").html("Please enter a valid domain");
+    }
+  });
 });
